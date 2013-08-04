b0d2023601117fa1dc1184e3b4a72b2bbfb8d75e
gabriel140492@gmail.com <gabriel140492@gmail.com@3eb108bd-aeac-dfc1-91e3-d43dfc70ff73>
2013-08-04 21:25:47 +0000

    Versao simples só com leitura e escrita no simulador.

--- Tan-k/src/tan/k/TanK.java
+++ Tan-k/src/tan/k/TanK.java
@@ -4,16 +4,57 @@
  */
 package tan.k;
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import br.ufrn.dca.controle.QuanserClient;
+import br.ufrn.dca.controle.QuanserClientException;
+
 /**
  *
  * @author sergio
  */
-public class TanK {
+public class TanK implements Runnable{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        QuanserClient quanserClient;
+      while(true){
+        try {
+          quanserClient = new QuanserClient("127.0.0.1", 20081);
+          double _read = quanserClient.read(0);
+          System.out.println("Leitura Canal 0: " + _read*6.25);
+          System.out.println("Gravando 0.8 volts no canal 4...");
+          quanserClient.write(0, 0.8);
+          Thread.sleep(1000);
+        } catch (QuanserClientException | InterruptedException ex) {
+          Logger.getLogger(TanK.class.getName()).log(Level.SEVERE, null, ex);
+        }
+      }
     }
+
+  @Override
+  public void run() {
+    QuanserClient quanserClient = null;
+      try {
+        quanserClient = new QuanserClient("127.0.0.1", 20081);
+      } catch (QuanserClientException ex) {
+        Logger.getLogger(TanK.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    while(true){
+      try {
+        double _read = 0.0;
+        if(null!=quanserClient) quanserClient.read(0);
+        System.out.println("Leitura Canal 0: " + _read);
+        Thread.sleep(300);
+        System.out.println("Gravando 1.2 volts no canal 4...");
+        if(null!=quanserClient) quanserClient.write(4, 1.2);
+        Thread.sleep(300);
+      } catch (QuanserClientException | InterruptedException ex) {
+        Logger.getLogger(TanK.class.getName()).log(Level.SEVERE, null, ex);
+      }
+    }
+  }
 }
